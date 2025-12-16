f01ff0e42cefeca89a3e519b5a50ecd89128c208
Heitor Lessa <heitor.lessa@hotmail.com>
2025-12-16 07:40:25 -0300

    refactor: allow echo port via env var

--- cmd/orr/main.go
+++ cmd/orr/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"github.com/heitorlessa/orr-sample/internal/api/handlers"
 	"github.com/labstack/echo/v4"
+	"os"
 )
 
 const STATIC_ASSETS = "web/static"
+const DEFAULT_PORT = ":8080"
 
 func main() {
 	e := echo.New()
 
 	e.Static("/static", STATIC_ASSETS)
 	e.GET("/", handlers.HomeHandler)
-	e.Logger.Fatal(e.Start(":8080"))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = DEFAULT_PORT
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
